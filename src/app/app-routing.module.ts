26edd2897fd0f7eb358692181b0a332bc93e444d
Vladimir Yussupov <v.yussupov@gmail.com>
2021-02-08 11:25:30 +0100

    Add recent changes
    Signed-off-by: Vladimir Yussupov <v.yussupov@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,9 @@ import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './shared/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', loadChildren: () => import(`src/app/home/home.module`).then(m => m.HomeModule)},
   {path: 'search', loadChildren: () => import(`src/app/search-page/search-page.module`).then(m => m.SearchPageModule)},
   {path: 'explorer', loadChildren: () => import(`src/app/explorer-page/explorer-page.module`).then(m => m.ExplorerPageModule)},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
 
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,7 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home.component';
 
 const infoPageRoutes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
