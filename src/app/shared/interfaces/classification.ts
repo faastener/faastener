e246eebf9059decdc6df99b8d10cbe9a3df7b4cf
Vladimir Yussupov <v.yussupov@gmail.com>
2021-04-09 09:40:29 +0200

    Update docs for classification interfaces
    Signed-off-by: Vladimir Yussupov <v.yussupov@gmail.com>

--- src/app/shared/interfaces/classification.ts
+++ src/app/shared/interfaces/classification.ts
@@ -1,35 +1,80 @@
 /**
- * An interface representing a generic classification framework, which comprises one or more {@link ClassificationViewCombination}
- * Different combinations of classification views can be used to flexibly switch between different representations.
+ * An interface representing a generic classification framework, which comprises one or more {@link ClassificationViewCombination}.
+ * Different combinations of classification views can be used to flexibly switch between different review representations for a given {@link TechnologyCategory}.
  */
 import {TechnologyCategory} from './technology';
 
 export interface ClassificationFramework {
+  /**
+   * Unique framework identifier.
+   */
   id: string;
+  /**
+   * Framework name.
+   */
   name: string;
+  /**
+   * Category of technologies this framework helps to review.
+   *
+   * @example
+   * Function-as-a-Service platforms
+   */
   technologyCategory: TechnologyCategory;
+  /**
+   * Framework description (optional).
+   */
   description?: string;
+  /**
+   * A set of {@link ClassificationViewCombination} which organize the representation of views comprising different criteria groupings.
+   */
   viewCombinations: Set<ClassificationViewCombination>;
 }
 
 /**
- * An interface representing a combination of classifications view which comprises one or more {@link ClassificationView}
+ * An interface representing a combination of classifications view which comprises one or more [ClassificationViews]{@link ClassificationView}.
  */
 export interface ClassificationViewCombination {
+  /**
+   * Unique view combination identifier.
+   */
   id: string;
+  /**
+   * View combination name.
+   */
   name: string;
+  /**
+   * View combination description.
+   */
   description: string;
+  /**
+   * A set of {@link ClassificationView} which organize the representation of criteria groupings.
+   */
   views: Set<ClassificationView>;
+  /**
+   * Indicates whether this combination needs to be used for default UI rendering (optional).
+   */
   default?: boolean;
 }
 
 /**
- * An interface representing a classification view which comprises one or more {@link CriteriaGrouping}
+ * An interface representing a classification view which comprises one or more {@link CriteriaGrouping}.
  */
 export interface ClassificationView {
+  /**
+   * Unique view identifier.
+   */
   id: string;
+  /**
+   * View name.
+   */
   name: string;
+  /**
+   * View description.
+   */
   description: string;
+  /**
+   * A set of {@link CriteriaGrouping} which organize the representation of criteria and optional sub-groupings.
+   */
   groupings: Set<CriteriaGrouping>;
 }
 
@@ -38,10 +83,29 @@ export interface ClassificationView {
  * Groupings can also contain other groupings, forming a hierarchy for criteria.
  */
 export interface CriteriaGrouping {
+  /**
+   * Unique grouping identifier.
+   */
   id: string;
-  locator: string; // e.g. "licensing" or "community.github"
+  /**
+   * Grouping locator which can be used, e.g., for custom breadcrumbs generation.
+   * Reserved for future use.
+   *
+   * @example
+   * "licensing", "community.github"
+   */
+  locator: string;
+  /**
+   * Grouping name.
+   */
   name: string;
+  /**
+   * A set of [Sub-groupings]{@link CriteriaGrouping} which organize the representation of criteria and optional sub-groupings.
+   */
   groupings?: Set<CriteriaGrouping>;
+  /**
+   * A set of {@link ClassificationCriterion} which describe a particular aspect of the reviewed technology.
+   */
   criteria?: Set<ClassificationCriterion>;
 }
 
@@ -52,20 +116,45 @@ export interface CriteriaGrouping {
  * @example a classification criterion representing runtimes supported by a FaaS platform
  */
 export interface ClassificationCriterion {
+  /**
+   * Unique criterion identifier.
+   */
   id: string;
+  /**
+   * Criterion name.
+   */
   name: string;
+  /**
+   * Criterion description.
+   */
   description: string;
+  /**
+   * Example values which an instance of this criterion {@link CriterionInstance} can store.
+   *
+   * @example
+   * a true/false value, numeric values, a list of strings
+   */
   exampleValues?: Set<string>;
 }
 
 /**
- * An interface representing an instance of a classification criterion containing the collected data for a particular target.
+ * An interface representing an instance of a classification criterion containing the collected data for a particular technology.
  *
  * @example a set of runtimes supported by AWS Lambda.
  */
 export interface CriterionInstance {
+  /**
+   * Unique {@link ClassificationCriterion} identifier.
+   */
   criterionId: string;
+  /**
+   * A set of {@link CriterionValue} which store the criterion-specific review data for the reviewed technology.
+   */
   values: Set<CriterionValue>;
+  /**
+   * A date review was conducted (optional).
+   * Reserved for future use.
+   */
   reviewDate?: Date;
 }
 
@@ -73,7 +162,19 @@ export interface CriterionInstance {
  * An interface representing a generic data value for the given {@link CriterionInstance}.
  */
 export class CriterionValue {
+  /**
+   * A value of primitive type representing criterion-specific review data.
+   *
+   * @example
+   * "Java", "not supported", 150, true
+   */
   value: string | number | boolean;
+  /**
+   * A list of references, e.g., to official documentation, confirming the review value (optional).
+   */
   references?: string[];
+  /**
+   * An additional note describing the review value (optional).
+   */
   comment?: string;
 }
